e3cfa97ef47554ccb7e302405552ac08aada217c
famithabegum <famithabegum418@gmail.com>
2025-12-04 15:45:31 +0530

    data types codes are added

--- variables and data types.py
+++ variables and data types.py
@@ -1,3 +1,59 @@
-a="aslam"
-b="famitha"
-print(a+b)
+# a="aslam"
+# b="famitha"
+# print(a+b)
+#
+# a=int("10")
+# b=int("100")
+# print(a+b)
+#
+# a=("12.")
+# b=("07.2025")
+# print(a+b)
+#
+# a=int (input())
+# b=int (input())
+# c=a+b
+# print(c)
+#
+# a = int(input())
+# b = int(input())
+# c = a - b
+# print(c)
+
+# a = input()
+# b = int(input())
+# print("My name is",a)
+# print("My age is",b)
+#
+# a = int(input())
+# b = int(input())
+# print("My name is",a)
+# print("My age is",b)
+# print(type(a))
+# print(type(b))
+# print(a+b)
+
+# Name=input()
+# Age=input()
+# Address=input()
+# print("My name is:",Name)
+# print("Age is:",Age)
+# print("Address is:",Address)
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=a+b+c
+# e=a*b*c
+# f=e/d
+# print(d)
+# print(e)
+# print(f)
+
+Name=input()
+Score=int(input())
+Department=input()
+Average=Score/10
+print(Name)
+print(Average)
+print(Department)
