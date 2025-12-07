d536428741a5efb8c10e5d05b80b88642834107e
famithabegum <famithabegum418@gmail.com>
2025-12-07 19:15:38 +0530

    if else codes are prcticised

--- If-else with Boolean values.py
+++ If-else with Boolean values.py
@@ -1,1 +1,330 @@
-Practicing Python code in If else Statement
+##Practicing Python code in If else Statement
+from reportlab.lib.pagesizes import elevenSeventeen
+
+# if(False):
+#     print("Yes")
+# else:
+#     print("No")
+#
+# Today="Rains"
+# if(Today=="Rain"):
+#     print("Yes It rains today")DIE
+# else:
+#     print("No It does not rain")
+#
+# Meghana=input()
+# if(Meghana=="died"):
+#     print("Surya meets Priya")
+# else:
+#     print("Surya marry meghana")
+#
+# Mark=int(input())
+# if(Mark>35):
+#     print("pass")
+# else:
+#     print("fail")
+
+# 
+# Income=int(input("Enter your income:"))
+# if(Income<7000):==
+#     print("Eligible for shcolarship")
+# else:
+#     print("Not eligible for scholarship")
+
+
+# number=int(input("Enter the number:"))
+# if(number%3==0 and number%5==0):
+#     print("number is divisible by 3 and 5")
+# else:
+#     print("number is not divisible")
+
+# a=int(input())
+# if(a%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# score=int(input("Enter your score: "))
+# if(score<=35):
+#     print("Poor student")
+# elif(score>35 and score<=70):
+#     print("Average student")
+# elif(score>70 and score<=100):
+#     print("Good student")
+# else:
+#     print("invalid input")
+
+# a = int(input("enter a: "))
+# b = int(input("enter b:"))
+# c = input("enter the operation add/sub/mul/div:")
+# if(c=="add"):
+#     print(a+b)
+# elif(c=="sub"):
+#     print(a-b)
+# elif(c=="mul"):
+#     print(a*b)
+# else:
+#     print(a/b)
+
+
+# a= int(input("enter your %:"))
+# if(a>=70):
+#     name=input("enter your name:")
+#     department=input("enter the department:")
+#     location=input("enter your location:")
+#     print("your are eligle")
+# else:
+#     print("you are not eligible")
+
+# salary=int(input("salary:"))
+# age=int(input("age:"))
+#
+# if(salary>=20000 or age<=25):
+#     amnt = int(input("loan amnt:"))
+#
+#     if(amnt<=50000):
+#         print("you are eligible for loan")
+#     else:
+#         print("maximum loan amnt is 50000")
+#
+# else:
+#     print("you are not eligible for loan")
+
+# a=int(input("enter a:"))
+# b=int(input("enter b:"))
+# c=int(input("enter c:"))
+# d=int(input("enter d:"))
+# e=int(input("enter e:"))
+# f=a+b+c+d+e
+# print(f)
+# g=f/5
+# print(g)
+# if(g<35):
+#     print("additional class is required")
+# else:
+#     print("you are good to go")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
